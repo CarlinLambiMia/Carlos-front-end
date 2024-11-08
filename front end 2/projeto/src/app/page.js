4b63dbd9da0fa1fd8bdf99df9457bba6cf531eea
CarlinLambiMia <CarlinLambiMia>
2024-11-08 08:26:25 -0400

    atualização com o sobre

--- front end 2/projeto/src/app/page.js
+++ front end 2/projeto/src/app/page.js
@@ -1,5 +1,6 @@
 //import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function Home() {
   const nome = 'carlos'
@@ -8,6 +9,7 @@ export default function Home() {
       <h1>Página principal</h1>
       <p>Paragrafo da pagina principal</p>
       <p>Nome: {nome}</p>
+      <Image className={styles.imagem} src='/images/download.jpg' alt='imagem aleatoria' width={600} height={400}></Image>
     </div>
   );
 }
